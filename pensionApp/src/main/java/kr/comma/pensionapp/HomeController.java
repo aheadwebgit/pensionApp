ae5bfce7133fbb9aed378c123bd495e5587aa900
tyhwang <tyhwang@tyhwang-PC>
2018-03-15 22:17:41 +0900

    menu정보 추가

--- pensionApp/src/main/java/kr/comma/pensionapp/HomeController.java
+++ pensionApp/src/main/java/kr/comma/pensionapp/HomeController.java
@@ -1,12 +1,13 @@
 package kr.comma.pensionapp;
 
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,15 +29,51 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+	public String index(HttpServletRequest request, HttpServletResponse response,Locale locale, Model model) {
+		try{
+			List<Map<String,Object>> dsMenuList = new ArrayList<Map<String,Object>>();
+			Map<String,Object> dsMap = new HashMap<String,Object>();
+			
+			dsMap = new HashMap<String,Object>();
+			dsMap.put("menuId", "01000000");
+			dsMap.put("menuNm", "펜션소개");
+			dsMap.put("menuEngNm", "About");
+			dsMap.put("menuUrl", "/about/");
+			dsMenuList.add(dsMap);
+			
+			dsMap = new HashMap<String,Object>();
+			dsMap.put("menuId", "02000000");
+			dsMap.put("menuNm", "객실안내");
+			dsMap.put("menuEngNm", "Room");
+			dsMap.put("menuUrl", "/room/");
+			dsMenuList.add(dsMap);
+			
+			dsMap = new HashMap<String,Object>();
+			dsMap.put("menuId", "03000000");
+			dsMap.put("menuNm", "부대시설 및 이벤트");
+			dsMap.put("menuEngNm", "Facilities & Event");
+			dsMap.put("menuUrl", "/facilities/");
+			dsMenuList.add(dsMap);
+			
+			dsMap = new HashMap<String,Object>();
+			dsMap.put("menuId", "04000000");
+			dsMap.put("menuNm", "예약안내");
+			dsMap.put("menuEngNm", "Reservation");
+			dsMap.put("menuUrl", "/reservation/");
+			dsMenuList.add(dsMap);
+			
+			dsMap = new HashMap<String,Object>();
+			dsMap.put("menuId", "05000000");
+			dsMap.put("menuNm", "커뮤니티");
+			dsMap.put("menuEngNm", "Community");
+			dsMap.put("menuUrl", "/community/");
+			dsMenuList.add(dsMap);
+			
+			request.getSession().setAttribute("menuList", dsMenuList);
+			
+		}catch(Exception e){
+			logger.error(e.getMessage());
+		}
 		
 		return "index";
 	}
